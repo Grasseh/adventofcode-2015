e3d432be9980e1563c362d21ed19356993db6714
Grasseh <steve.gagnev4si@gmail.com>
2015-12-04 13:14:37 -0500

    Solved problem 4b

--- problem4.py
+++ problem4.py
@@ -5,7 +5,7 @@ found = False
 while not found:
   hash = hashlib.md5()
   hash.update(input + str(i))
-  found = hash.hexdigest()[:5] == "00000"
+  found = hash.hexdigest()[:6] == "000000"
   if not found:
     i += 1
-print i, hash.hexdigest(), input + str(i), hash.hexdigest()[:5] == "00000", found
+print i, hash.hexdigest(), input + str(i), hash.hexdigest()[:6] == "00000", found
