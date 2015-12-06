bf160d2c5fe14c2837777add1edb7cd315d58e92
Grasseh <steve.gagnev4si@gmail.com>
2015-12-06 16:43:50 -0500

    Solved problem 6b

--- problem6.py
+++ problem6.py
@@ -7,7 +7,7 @@ grid = []
 for i in range(0, 1000):
   grid.append([])
   for j in range(0, 1000):
-    grid[i].append(False)
+    grid[i].append(0)
 #Run commands
 for command in array:
   firstSeparator = command.find(",")
@@ -19,20 +19,21 @@ for command in array:
     x1 = command[8:firstSeparator]
     for i in range(int(x1),int(x2)+1):
       for j in range(int(y1),int(y2)+1):
-        grid[i][j] = True
+        grid[i][j] += 1
   elif command.find("turn off") != -1:
     x1 = command[9:firstSeparator]
     for i in range(int(x1),int(x2)+1):
       for j in range(int(y1),int(y2)+1):
-        grid[i][j] = False
+        if grid[i][j] > 0:
+          grid[i][j] -= 1
   elif command.find("toggle") != -1:
     x1 = command[7:firstSeparator]
     for i in range(int(x1),int(x2)+1):
       for j in range(int(y1),int(y2)+1):
-        grid[i][j] = not grid[i][j]
+        grid[i][j] += 2
 
 #Count
 for i in range(0,1000):
   for j in range(0,1000):
-    value += grid[i][j] if 1 else 0
+    value += grid[i][j]
 print value
