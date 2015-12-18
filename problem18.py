bb47392e960c03d1ff0d74d878c9e0bf92f1298c
Grasseh <steve.gagnev4si@gmail.com>
2015-12-18 10:49:07 -0500

    Solved problem 18a

--- problem18.py
+++ problem18.py
@@ -3,10 +3,10 @@ import re
 import copy
 string = input()
 array = string.splitlines()
-value = "0"
+value = 0
 lights = []
-size = 6
-steps = 0
+size = 100
+steps = 100
 
 class Light:
   def __init__(self,on):
@@ -26,7 +26,33 @@ for i in range(0,steps):
   #Setup each next
   for i in range(0,size):
     for j in range(0,size):
-      lights[i][j].next = False
+      #COUNT all eight neighbours
+      count = 0
+      if i > 0:
+        count += 1 if lights[i-1][j].on else 0
+        if j > 0:
+          count += 1 if lights[i-1][j-1].on else 0
+        if j < size - 1:
+          count += 1 if lights[i-1][j+1].on else 0
+      if i < size - 1:
+        count += 1 if lights[i+1][j].on else 0
+        if j > 0:
+          count += 1 if lights[i+1][j-1].on else 0
+        if j < size - 1:
+          count += 1 if lights[i+1][j+1].on else 0
+      if j > 0:
+        count += 1 if lights[i][j-1].on else 0
+      if j < size - 1:
+        count += 1 if lights[i][j+1].on else 0
+
+
+      if lights[i][j].on:
+        #If on, check if 2 or 3 neighbours are on
+        lights[i][j].next = count in [2,3]
+      else:
+        #If off, check if EXACTLY 3 neighbours are on
+        lights[i][j].next = count in [3]
+
   #Put each on at next
   for i in range(0,size):
     for j in range(0,size):
@@ -38,6 +64,7 @@ for i in range(0,size):
   string = ""
   for j in range(0,size):
     string += "#" if lights[i][j].on else "."
+    value += 1 if lights[i][j].on else 0
   print string
 
 print "answer" , value
